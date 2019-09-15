e4c98bde2ced33dee475f55659376664dbc240a4
Igor Camões <itscamoes@gmail.com>
2019-09-15 12:18:40 -0300

    Create fetch function and JSX structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [drinks, setDrinks] = useState([]);
+
+  const getRandomDrink = async () =>{
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const drinkData = await response.json();
+    setDrinks(drinkData.drinks);
+    const drinkDisplayE = document.querySelector('.drinkDisplay');
+    drinkDisplayE.style.animation = 'anim1 0.4s ease-in-out';
+  };
+
+  useEffect(() =>{
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <main>
+        <button onClick={getRandomDrink}>Random</button>
+        <input type="text" placeholder="Search for a drink"/>
+        <button type="submit">Search</button>
+      </main>
+    
+      <div>
+
+      {drinks.map(drink =>(
+        <div className='drinkDisplay'>
+          <h2 key={drink.drinkId}>{drink.strDrink}</h2>
+          <div>
+            <img className='drinkImg' src={drink.strDrinkThumb} alt=""/>
+          </div>
+          <p>{drink.strGlass} - {drink.strAlcoholic}</p>
+        </div>
+      ))}
+
+      </div>
+
+      </div>
   );
 }
 
