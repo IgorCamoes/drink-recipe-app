d072aed69a4b06e04a300dac3657b3476a2cd75b
Igor Camões <itscamoes@gmail.com>
2019-09-19 20:54:47 -0300

    Updates

---
+++ precache-manifest.632f4e664e8ec80edf72f56cd631fc4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01887b8f4c342a5e59aabe5271157f0",
+    "url": "/drink-recipe-app/index.html"
+  },
+  {
+    "revision": "64833d83647233a5b947",
+    "url": "/drink-recipe-app/static/css/main.41f57fa0.chunk.css"
+  },
+  {
+    "revision": "ac44f06c5aef34e80aa0",
+    "url": "/drink-recipe-app/static/js/2.5537d399.chunk.js"
+  },
+  {
+    "revision": "64833d83647233a5b947",
+    "url": "/drink-recipe-app/static/js/main.900b2e60.chunk.js"
+  },
+  {
+    "revision": "023f61cce5e4c713c16c",
+    "url": "/drink-recipe-app/static/js/runtime~main.a2d3bf3f.js"
+  },
+  {
+    "revision": "7a45aee8a11199f55692f5889eef83e5",
+    "url": "/drink-recipe-app/static/media/bg1.7a45aee8.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/drink-recipe-app/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "698ddeaff4f24bad176f0afa453c5bb3",
+    "url": "/drink-recipe-app/static/media/linkedin.698ddeaf.svg"
+  }
+]);
